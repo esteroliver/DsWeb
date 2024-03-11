c05acbe3ed98d5ae259b457474c58eba84f846ab
esteroliver <oliver.ester.2004@gmail.com>
2024-03-11 13:57:23 +0000

    banco de dados

--- meusite/enquetes/urls.py
+++ meusite/enquetes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('enquetes/', views.index, name='index'),
+    path('', views.index, name='index'),
     #enquetes - endereço (localhost:8000/enquetes)
     #views.index - função que vai ser ativada nesse endereço
 ]
