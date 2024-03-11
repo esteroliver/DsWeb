07a9ac765d827dd37e6e2f6f1d663057baf68662
ESTER OLIVEIRA DO NASCIMENTO DUARTE MELO <20231014040002@ifrn.local>
2024-03-11 11:41:21 -0300

    correção

--- meusite/enquetes/models.py
+++ meusite/enquetes/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Pergunta(models.Model):
-    texto = models.CharField(max_lenght=200)
+    texto = models.CharField(max_length=200)
     data_pub = models.DateTimeField('Data de publicação')
 
 class Alternativa(models.Model):
-    texto = models.CharField(max_lenght=200)
+    texto = models.CharField(max_length=200)
     votos = models.IntegerField(default=0)
     pergunta_ass = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
 
