91ec6543395ebaaa5207027508642081a6cf3445
ESTER OLIVEIRA DO NASCIMENTO DUARTE MELO <20231014040002@ifrn.local>
2024-03-14 09:42:15 -0300

    função def

--- meusite/enquetes/models.py
+++ meusite/enquetes/models.py
@@ -3,9 +3,13 @@ from django.db import models
 class Pergunta(models.Model):
     texto = models.CharField(max_length=200)
     data_pub = models.DateTimeField('Data de publicação')
+    def __self__(self):
+        return self.texto
 
 class Alternativa(models.Model):
     texto = models.CharField(max_length=200)
     votos = models.IntegerField(default=0)
     pergunta_ass = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    def __self__(self):
+        return self.texto
 
