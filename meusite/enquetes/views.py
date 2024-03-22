74f064e59f86520d1e8ca9f0ea3a1af41ae6eb40
esteroliver <oliver.ester.2004@gmail.com>
2024-03-22 12:40:04 +0000

    template de detalhes

--- meusite/enquetes/views.py
+++ meusite/enquetes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from .models import Pergunta
 
 def index(request):
@@ -8,8 +9,11 @@ def index(request):
     return render(request, 'enquetes/index.html', contexto)
 
 def detalhes(request, pergunta_id):
-    resultado = "DETALHES da pergunta - %s"
-    return HttpResponse(resultado % pergunta_id)
+    try:
+        pergunta = Pergunta.objects.get(pk=pergunta_id)
+    except Pergunta.DoesNotExist:
+        raise Http404('Pergunta não existe.')
+    return render(request, 'enquetes/detalhes.html', {'pergunta':pergunta})
 
 def votacao(request, pergunta_id):
     resultado = "VOTAÇÃO da pergunta - %s"
