d86fb5e3c35f8166f2bc1a77590ac9e42138d325
esteroliver <oliver.ester.2004@gmail.com>
2024-04-04 13:35:27 +0000

    criando votacao e resultado

--- meusite/enquetes/urls.py
+++ meusite/enquetes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'enquetes'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:pergunta_id>/', views.detalhes, name='detalhes'),
-    path('<int:pergunta_id>/votacao/', views.votacao, name='votação'),
+    path('<int:pergunta_id>/votacao/', views.votacao, name='votacao'),
     path('<int:pergunta_id>/resultado/', views.resultado, name='resultado'),
 ]
--- meusite/enquetes/views.py
+++ meusite/enquetes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import Http404
-from .models import Pergunta
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Pergunta, Alternativa
+from django.urls import reverse
 
 def index(request):
     lista_ult_perguntas = Pergunta.objects.all()
@@ -16,10 +16,29 @@ def detalhes(request, pergunta_id):
     return render(request, 'enquetes/detalhes.html', {'pergunta':pergunta})
 
 def votacao(request, pergunta_id):
-    resultado = "VOTAÇÃO da pergunta - %s"
-    return HttpResponse(resultado % pergunta_id)
+    try:
+        pergunta = Pergunta.objects.get(pk=pergunta_id)
+    except Pergunta.DoesNotExist:
+        raise Http404('Pergunta não existe.') #pegando a pergunta
+
+    try:
+        pergunta_selecionada = pergunta.alternativa_set.get(pk=request.POST['voto']) #pergunta_selecionada vai receber uma alternativa que foi enviada pelo método post
+
+    except (KeyError, Alternativa.DoesNotExist): #caso haja um KeyError (não existe o id) ou a pergunta não exista, haverá essa exceção
+        contexto = {'pergunta':pergunta,
+        'error_message':"Selecione uma opção"}
+        return render(request, 'enquetes/detalhes.html', contexto) #caso um usuário não selecione uma opção
+
+    else: #caso não entre no except
+        pergunta_selecionada.votos += 1
+        pergunta_selecionada.save() #quando alteramos o estado de um objeto, temos que salvar
+        return HttpResponseRedirect(reverse('enquetes:resultado', args=(pergunta_id,))) #redirecionando para outra view (resultado) com o parâmetro pergunta_id, que faz parte de uma coleção
+                               #   '/enquetes/pergunta_id/resultado'
+    #não é necessário passar o request por causa do protocolo Http
 
 def resultado(request, pergunta_id):
-    resultado = "RESULTADO da pergunta - %s"
-    return HttpResponse(resultado % pergunta_id)
-
+    try:
+        pergunta = Pergunta.objects.get(pk=pergunta_id)
+    except Pergunta.DoesNotExist:
+        raise Http404('Pergunta não existe.')
+    return render(request, 'enquetes/resultado.html', {'pergunta':pergunta})
