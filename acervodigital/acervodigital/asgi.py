4e76f09f41f261822d95bcc3198958a4f85b1876
esteroliver <oliver.ester.2004@gmail.com>
2024-08-02 13:26:41 +0000

    acervo digital

--- acervodigital/acervodigital/asgi.py
+++ acervodigital/acervodigital/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for peidigital project.
+ASGI config for acervodigital project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'peidigital.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acervodigital.settings')
 
 application = get_asgi_application()
