3995847bfaa14775dd3f1791453ef1af0deb5e0c
esteroliver <oliver.ester.2004@gmail.com>
2024-09-13 01:29:52 +0000

    acervo digital

--- meusite/acervodigital/urls.py
+++ meusite/acervodigital/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.CadastroView.as_view(), name='cadastro'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('index/', views.IndexView.as_view(), name='index'),
 ]
--- meusite/acervodigital/views.py
+++ meusite/acervodigital/views.py
@@ -1,6 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-from .models import Livro
+from .models import Livro, Usuario
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+class LoginView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'acervodigital/login.html')
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        user = User.objects.get(email=email)
+        if user != 0:
+            user = authenticate(request, username=user.username, password=senha)
+            login(request, user)
+            return redirect('/index')
+        else:
+            messages.error(request, 'Usuario não existe')
+
+class CadastroView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'acervodigital/cadastro.html')
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST['email']
+        senha = request.POST['senha']
+        nome = request.POST['nome']
+        username = request.POST['username']
+
+        if User.objects.filter(username=username).exists():
+            messages.error("Já existe um usuário com esse username")
+        elif User.objects.filter(email=email).exists():
+            messages.error("Já existe um usuário com esse email")
+        else:
+            user = User.objects.create_user(
+                username = username,
+                email = email,
+                first_name = nome,
+                password = senha
+            )
+            user.save()
+            usuario = Usuario.objects.create(
+                user = user
+            )
+            usuario.save()
+            return redirect('/login')
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
